a3b22b5ead4d2590f67d12362663b17ca6c57364
Robert Miller <miller.robert.john@gmail.com>
2021-12-10 10:46:46 -0700

    Show message when no posts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,24 +2,41 @@
 
 @section('content')
     <h1 class="display-3 pb-3">Astronomy Pictures of the Day</h1>
+
     @if( Request::routeIs('index.date'))
         <h2 class="pb-5">{{ date('F j, Y', strtotime($date)) }}</h2>
     @endif
-    @foreach($posts as $post)
-        <div class="row mt-3 pb-5">
-            <div class="col">
-                <h3><a style="color: #212529 !important" href="{{ route('index.post', $post->id) }}">{{ $post->title }}</a></h3>
-                <h6>Posted by {{ $users[$post->id] }}</h6>
-                <p class="mt-n2">{{ $post->date }}</p>
-                <img class="img-fluid" src="{{ $post->img_url }}">
-                <figcaption class="figure-caption">
-                    {{ $post->copyright ? " (Copyright {$post->copyright})" : '' }}
-                </figcaption>
-                <p class="mt-3">{{ $post->explanation }}</p>
-                <small class="pb-3">(Originally posted on <a href="http://apod.nasa.gov/apod/astropix.html">NASA APOD</a> {{ $post->original_date }})</small>
+
+    @if($posts)
+        @foreach($posts as $post)
+            <div class="row mt-3 pb-5">
+                <div class="col">
+                    <h3><a style="color: #212529 !important" href="{{ route('index.post', $post->id) }}">{{ $post->title }}</a></h3>
+
+                    <h6>Posted by {{ $users[$post->id] }}</h6>
+
+                    <p class="mt-n2">{{ $post->date }}</p>
+
+                    <img class="img-fluid" src="{{ $post->img_url }}">
+
+                    <figcaption class="figure-caption">
+                        {{ $post->copyright ? " (Copyright {$post->copyright})" : '' }}
+                    </figcaption>
+
+                    <p class="mt-3">{{ $post->explanation }}</p>
+
+                    <small class="pb-3">(Originally posted on <a href="http://apod.nasa.gov/apod/astropix.html">NASA APOD</a> {{ $post->original_date }})</small>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    @else
+            <div class="row mt-3 pb-5">
+                <div class="col">
+                    <h3 class="font-weight-normal"><em>No Posts</em></h3>
+                </div>
+            </div>
+    @endif
+
     <div class="row mt-5 pb-5">
         <div class="col-md-12 text-center">
             {{ $posts->links() }}
